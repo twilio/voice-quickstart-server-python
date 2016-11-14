9c7f1af6bae8c43e9db4ae35adb5499298d63df1
hieplebstar <hiep.le@bstarsolutions.com>
2016-11-14 17:08:16 +0700

    Update server.py

--- server.py
+++ server.py
@@ -68,7 +68,7 @@ def callLog():
   client = Client(api_key, api_key_secret, account_sid)
   for call in client.calls.list(to="client:"+IDENTITY):
     print("From: " + call.from_formatted + " To: " + call.to_formatted)
-  return
+  return str("")
 
 @app.route('/', methods=['GET', 'POST'])
 def welcome():
