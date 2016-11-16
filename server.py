2a5cb86cc9fb751fa5c754cc0748833b9fa57af9
hieplebstar <hiep.le@bstarsolutions.com>
2016-11-16 14:17:14 +0700

    Update server.py

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ API_KEY_SECRET = 'BM9ohnBeTGb3z9NJFGhsigG7TeDxhjCl'
 PUSH_CREDENTIAL_SID = 'CR17e1e242557f010ad8739c0ccabe29bb'
 APP_SID = 'AP5dadf62e0240fad9dc927308b7dadc46'
 AUTH_TOKEN = 'e1b308b76f84d974c871bb87719249bc'
-CONTAST_LIST = {['userName': 'Twilio Demo 1'], ['userName': 'Twilio Demo 2'], ['userName': 'Twilio Demo 3'], ['userName': 'Twilio Demo 4'], ['userName': 'Twilio Demo 5'], ['userName': 'Twilio Demo 6'], ['userName': 'Twilio Demo 7'], ['userName': 'Twilio Demo 8'], ['userName': 'Twilio Demo 9'], ['userName': 'Twilio Demo 10'], ['userName': 'Twilio Demo 11'], ['userName': 'Twilio Demo 12'], ['userName': 'Twilio Demo 13'], ['userName': 'Twilio Demo 14'], ['userName': 'Twilio Demo 15']}
+CONTAST_LIST = [{'userName': 'Twilio Demo 1'}, {'userName': 'Twilio Demo 2'}, {'userName': 'Twilio Demo 3'}, {'userName': 'Twilio Demo 4'}, {'userName': 'Twilio Demo 5'}, {'userName': 'Twilio Demo 6'}, {'userName': 'Twilio Demo 7'}, {'userName': 'Twilio Demo 8'}, {'userName': 'Twilio Demo 9'}, {'userName': 'Twilio Demo 10'}, {'userName': 'Twilio Demo 11'}, {'userName': 'Twilio Demo 12'}, {'userName': 'Twilio Demo 13'}, {'userName': 'Twilio Demo 14'}, {'userName': 'Twilio Demo 15'}]
 
 IDENTITY = 'voice_test'
 CALLER_ID = 'quick_start'
