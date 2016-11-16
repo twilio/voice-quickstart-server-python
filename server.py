b198d9597cc56ccf4131c4592162ce98bb7020ff
hieplebstar <hiep.le@bstarsolutions.com>
2016-11-17 05:24:26 +0700

    Update server.py

--- server.py
+++ server.py
@@ -8,7 +8,8 @@ import twilio.twiml
 ACCOUNT_SID = 'AC64d6a7fa1eb934e5ed068f57e4ac16f9'
 API_KEY = 'SK798c3d34afc34d650b1465606ccf640e'
 API_KEY_SECRET = 'BM9ohnBeTGb3z9NJFGhsigG7TeDxhjCl'
-PUSH_CREDENTIAL_SID = 'CR54d55c09309224b87d11516c6d7ec23d'
+PUSH_CREDENTIAL_SID_IOS = 'CR54d55c09309224b87d11516c6d7ec23d'
+PUSH_CREDENTIAL_SID_ANDROID = 'CR17e1e242557f010ad8739c0ccabe29bb'
 APP_SID = 'AP5dadf62e0240fad9dc927308b7dadc46'
 AUTH_TOKEN = 'e1b308b76f84d974c871bb87719249bc'
 CONTACT_LIST = [{'userName': 'Jacob'}, {'userName': 'Stephanie'}, {'userName': 'Samita'}, {'userName': 'MoonShik'}, {'userName': 'Laurel'}, {'userName': 'Chelsea'}, {'userName': 'John'}, {'userName': 'Michael'}, {'userName': 'Andrew'}, {'userName': 'Kathryn'}, {'userName': 'Caleb'}, {'userName': 'Emily'}, {'userName': 'Isaac'}, {'userName': 'Hannah'}, {'userName': 'Barrett'}, {'userName': 'Elizabeth'}, {'userName': 'Dana'}, {'userName': 'Alisha'}, {'userName': 'Lewis'}, {'userName': 'Emma'}, {'userName': 'Quinn'}, {'userName': 'Benjamin'}, {'userName': 'Bianca'}, {'userName': 'Philip'}, {'userName': 'Kathryn'}, {'userName': 'Allison'}, {'userName': 'Jackson'}, {'userName': 'Oscar'}, {'userName': 'Corine'}, {'userName': 'Benjamin'}]
@@ -21,11 +22,17 @@ app = Flask(__name__)
 @app.route('/accessToken', methods=['GET', 'POST'])
 def token():
   client_name = request.values.get('client')
+  platform = request.values.get('platform')
   account_sid = os.environ.get("ACCOUNT_SID", ACCOUNT_SID)
   api_key = os.environ.get("API_KEY", API_KEY)
   api_key_secret = os.environ.get("API_KEY_SECRET", API_KEY_SECRET)
-  push_credential_sid = os.environ.get("PUSH_CREDENTIAL_SID", PUSH_CREDENTIAL_SID)
   app_sid = os.environ.get("APP_SID", APP_SID)
+  
+  if platform == 'ios':
+    push_credential_sid = os.environ.get("PUSH_CREDENTIAL_SID_IOS", PUSH_CREDENTIAL_SID_IOS)
+  else:
+    push_credential_sid = os.environ.get("PUSH_CREDENTIAL_SID_ANDROID", PUSH_CREDENTIAL_SID_ANDROID)
+    
   if client_name:
      IDENTITY = client_name
   grant = VoiceGrant(
