73429e1c2e4c06629676fb5ebe0ab6280b9b5759
hieplebstar <hiep.le@bstarsolutions.com>
2016-11-25 15:38:11 +0700

    Update server.py

--- server.py
+++ server.py
@@ -3,16 +3,15 @@ import json
 from flask import Flask, request
 from twilio.jwt.access_token import AccessToken, VoiceGrant
 from twilio.rest import Client
-from flask import url_for
 import twilio.twiml
 
-ACCOUNT_SID = 'AC64d6a7fa1eb934e5ed068f57e4ac16f9'
-API_KEY = 'SK798c3d34afc34d650b1465606ccf640e'
-API_KEY_SECRET = 'BM9ohnBeTGb3z9NJFGhsigG7TeDxhjCl'
+ACCOUNT_SID = 'AC930ff2e97f313c236fe571db9987e5a0'
+API_KEY = 'SKc0f6bc9d50890b39ee18a597bfd1f6a7'
+API_KEY_SECRET = '5ALedwHKH9P2FzXSxCuwC4KZqp3MDvEb'
 PUSH_CREDENTIAL_SID_IOS = 'CR46ae38c939adf11bee2ecdc8e9e4a0b1'
-PUSH_CREDENTIAL_SID_ANDROID = 'CR1d973685761d00a2d4cef10a88c2391d'
-APP_SID = 'AP5dadf62e0240fad9dc927308b7dadc46'
-AUTH_TOKEN = 'e1b308b76f84d974c871bb87719249bc'
+PUSH_CREDENTIAL_SID_ANDROID = 'CR0776cf10fe89986c7d40628e532dcd31'
+APP_SID = 'APaafd4dcb530c4c3811e85738f4df3f7f'
+AUTH_TOKEN = 'f2f4669503e0ca0b04a8ed3f50bb489b'
 CONTACT_LIST = [{'userName': 'Phong'}, {'userName': 'Dinh'}, {'userName': 'Antony'}, {'userName': 'Khanh'}, {'userName': 'Tan'}, {'userName': 'Hiep'}, {'userName': 'Thuc'}, {'userName': 'Tam'}, {'userName': 'Phi'}, {'userName': 'Jacob'}, {'userName': 'Stephanie'}, {'userName': 'Samita'}, {'userName': 'MoonShik'}, {'userName': 'Laurel'}, {'userName': 'Chelsea'}, {'userName': 'John'}, {'userName': 'Michael'}, {'userName': 'Andrew'}, {'userName': 'Kathryn'}, {'userName': 'Caleb'}, {'userName': 'Emily'}, {'userName': 'Isaac'}, {'userName': 'Hannah'}, {'userName': 'Barrett'}, {'userName': 'Elizabeth'}, {'userName': 'Dana'}, {'userName': 'Alisha'}, {'userName': 'Lewis'}, {'userName': 'Emma'}, {'userName': 'Quinn'}, {'userName': 'Benjamin'}, {'userName': 'Bianca'}, {'userName': 'Philip'}, {'userName': 'Kathryn'}, {'userName': 'Allison'}, {'userName': 'Jackson'}, {'userName': 'Oscar'}, {'userName': 'Corine'}, {'userName': 'Benjamin'}]
 
 IDENTITY = 'voice_test'
@@ -46,40 +45,35 @@ def token():
 
   return str(token)
 
-@app.route('/incoming', methods=['GET', 'POST'])
-def incoming():
-  resp = twilio.twiml.Response()
-  resp.say("Congratulations! You have received your first inbound call! Good bye.")
-  return str(resp)
-
 @app.route('/outbound', methods=['POST'])
 def outbound():
-    response = twilio.twiml.Response()
+    response = twiml.Response()
     with response.dial() as dial:
-        dial.client(CALLER_ID)
+        dial.number("+16518675309")
     return str(response)
   
 @app.route('/outgoing', methods=['GET', 'POST'])
 def outgoing():
+  resp = twilio.twiml.Response()
   from_value = request.values.get('From')
-  to_value = request.values.get('To')
-  account_sid = os.environ.get("ACCOUNT_SID", ACCOUNT_SID)
-  api_key = os.environ.get("API_KEY", API_KEY)
-  api_key_secret = os.environ.get("API_KEY_SECRET", API_KEY_SECRET)
-
-  client = Client(api_key, api_key_secret, account_sid)
-  call = client.calls.create(url='http://twimlets.com/holdmusic?Bucket=com.twilio.music.ambient', to=''+to_value, from_=''+from_value)
-  return str(call.sid)
-
-@app.route('/placeCall', methods=['GET', 'POST'])
-def placeCall():
-  account_sid = os.environ.get("ACCOUNT_SID", ACCOUNT_SID)
-  api_key = os.environ.get("API_KEY", API_KEY)
-  api_key_secret = os.environ.get("API_KEY_SECRET", API_KEY_SECRET)
-
-  client = Client(api_key, api_key_secret, account_sid)
-  call = client.calls.create(url=request.url_root + 'incoming', to='client:' + 'hiep', from_='client:' + 'thuc')
-  return str(call.sid)
+  to = request.values.get('To')
+  if not (from_value and to):
+    resp.say("Invalid request")
+    return str(resp)
+  from_client = from_value.startswith('client')
+  caller_id = os.environ.get("CALLER_ID", CALLER_ID)
+  if not from_client:
+    # PSTN -> client
+    resp.dial(callerId=from_value).client(CLIENT)
+  elif to.startswith("client:"):
+    # client -> client
+    resp.dial(callerId=from_value).client(to[7:])
+  else:
+    # client -> PSTN
+    resp.dial(to, callerId=caller_id)
+  # if call end or failed
+  # resp.say("The call failed, or the remote party hung up. Goodbye.")
+  return str(resp)
 
 @app.route('/callLog', methods=['GET', 'POST'])
 def callLog():
