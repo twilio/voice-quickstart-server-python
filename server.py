f9857a2576c7535d5be4765637d5596182058069
hieplebstar <hiep.le@bstarsolutions.com>
2016-11-25 14:11:49 +0700

    Update server.py

--- server.py
+++ server.py
@@ -5,13 +5,13 @@ from twilio.jwt.access_token import AccessToken, VoiceGrant
 from twilio.rest import Client
 import twilio.twiml
 
-ACCOUNT_SID = 'AC930ff2e97f313c236fe571db9987e5a0'
-API_KEY = 'SKc0f6bc9d50890b39ee18a597bfd1f6a7'
-API_KEY_SECRET = '5ALedwHKH9P2FzXSxCuwC4KZqp3MDvEb'
+ACCOUNT_SID = 'AC64d6a7fa1eb934e5ed068f57e4ac16f9'
+API_KEY = 'SK798c3d34afc34d650b1465606ccf640e'
+API_KEY_SECRET = 'BM9ohnBeTGb3z9NJFGhsigG7TeDxhjCl'
 PUSH_CREDENTIAL_SID_IOS = 'CR46ae38c939adf11bee2ecdc8e9e4a0b1'
-PUSH_CREDENTIAL_SID_ANDROID = 'CR0776cf10fe89986c7d40628e532dcd31'
-APP_SID = 'APaafd4dcb530c4c3811e85738f4df3f7f'
-AUTH_TOKEN = 'f2f4669503e0ca0b04a8ed3f50bb489b'
+PUSH_CREDENTIAL_SID_ANDROID = 'CR1d973685761d00a2d4cef10a88c2391d'
+APP_SID = 'AP5dadf62e0240fad9dc927308b7dadc46'
+AUTH_TOKEN = 'e1b308b76f84d974c871bb87719249bc'
 CONTACT_LIST = [{'userName': 'Phong'}, {'userName': 'Dinh'}, {'userName': 'Antony'}, {'userName': 'Khanh'}, {'userName': 'Tan'}, {'userName': 'Hiep'}, {'userName': 'Thuc'}, {'userName': 'Tam'}, {'userName': 'Phi'}, {'userName': 'Jacob'}, {'userName': 'Stephanie'}, {'userName': 'Samita'}, {'userName': 'MoonShik'}, {'userName': 'Laurel'}, {'userName': 'Chelsea'}, {'userName': 'John'}, {'userName': 'Michael'}, {'userName': 'Andrew'}, {'userName': 'Kathryn'}, {'userName': 'Caleb'}, {'userName': 'Emily'}, {'userName': 'Isaac'}, {'userName': 'Hannah'}, {'userName': 'Barrett'}, {'userName': 'Elizabeth'}, {'userName': 'Dana'}, {'userName': 'Alisha'}, {'userName': 'Lewis'}, {'userName': 'Emma'}, {'userName': 'Quinn'}, {'userName': 'Benjamin'}, {'userName': 'Bianca'}, {'userName': 'Philip'}, {'userName': 'Kathryn'}, {'userName': 'Allison'}, {'userName': 'Jackson'}, {'userName': 'Oscar'}, {'userName': 'Corine'}, {'userName': 'Benjamin'}]
 
 IDENTITY = 'voice_test'
