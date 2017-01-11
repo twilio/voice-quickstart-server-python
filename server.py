8474c10e90a2f4eec29f1bae105cef2f3167cfb7
hieplebstar <hiep.le@bstarsolutions.com>
2017-01-11 15:30:08 +0700

    Update server.py

--- server.py
+++ server.py
@@ -110,7 +110,7 @@ def verification():
   
   phoneNumber = request.values.get('phoneNumber')
   friendlyName = request.values.get('friendlyName')
-  new_phone = client.account.phone_numbers.create(phone_number=phoneNumber)
+  new_phone = client.caller_ids.validate(phoneNumber, friendly_name=friendlyName)
   return new_phone.validation_code
 
 @app.route('/callLog', methods=['GET', 'POST'])
