7291c620b76fa9fdb59a04a43748838743a6ba41
hieplebstar <hiep.le@bstarsolutions.com>
2017-01-11 14:52:18 +0700

    Update server.py

--- server.py
+++ server.py
@@ -103,10 +103,9 @@ def outgoing():
 @app.route('/verification', methods=['GET', 'POST'])
 def verification():
   account_sid = os.environ.get("ACCOUNT_SID", ACCOUNT_SID)
-  api_key = os.environ.get("API_KEY", API_KEY)
-  api_key_secret = os.environ.get("API_KEY_SECRET", API_KEY_SECRET)
+  auth_token = os.environ.get("AUTH_TOKEN", AUTH_TOKEN)
 
-  client = Client(api_key, api_key_secret, account_sid)
+  client = Client(account_sid, auth_token)
   
   phoneNumber = request.values.get('phoneNumber')
   friendlyName = request.values.get('friendlyName')
