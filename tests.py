904b62d5d01c58b6b52dd75f7c33275c538e0ada
idelgado <idelgado@twilio.com>
2018-01-29 21:34:38 -0500

    Remove newline

--- tests.py
+++ tests.py
@@ -155,7 +155,6 @@ class TestRoutes(unittest.TestCase):
         self.assertTrue(self.DEFAULT_NUMBER_CALLER_ID in r.text)
         self.assertTrue(self.validate_number_in_xml(number, self.DEFAULT_NUMBER_CALLER_ID, r.text))
 
-
     def validate_number_in_xml(self, number, callerId, xml):
         simple_number_xml = "<Number>" + number + "</Number>"
         dialer_number_xml = "<Number callerId=\"" + callerId + "\">" + number + "</Number>"
